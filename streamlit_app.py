b8ef077ca43d99b0d2834ef8546cad58be497fb7
jumarti96evolv <juan.martinez@evolvconsulting.com>
2025-07-28 14:12:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,6 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon='✅')
 
-smoothiefroot_response = requests.get('https://smoothiefroot.com/api/fruit/watermelon')
+smoothiefroot_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 #st.text(smoothiefroot_response.json(), use_container_width=True)
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
