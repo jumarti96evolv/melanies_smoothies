0ec229e93cb3db228f2b0afa24d216f2baef88bf
jumarti96evolv <juan.martinez@evolvconsulting.com>
2025-07-28 14:09:04 -0500

    Update streamlit_app.py
    Update to the API call implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon='✅')
 
 smoothiefroot_response = requests.get('https://smoothiefroot.com/api/fruit/watermelon')
-st.text(smoothiefroot_response.json(), use_container_width=True)
+#st.text(smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
