6a4e369699348dc61ddb9abddc5ab1c9ff05f512
jumarti96evolv <juan.martinez@evolvconsulting.com>
2025-07-29 16:00:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         smoothiefroot_response = requests.get('https://fruityvice.com/api/fruit/' + search_on)
 
         st.subheader(fruit_chosen + ' Nutrition Information')
