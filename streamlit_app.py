91bdcad87a03d7c1d3176fbf77f095e3e43d2f1a
jumarti96evolv <juan.martinez@evolvconsulting.com>
2025-07-29 15:59:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,16 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-st.stop()
-
 if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_chosen)
+
+        search_on = pd_df.loc[['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        smoothiefroot_response = requests.get('https://fruityvice.com/api/fruit/' + search_on)
+
+        st.subheader(fruit_chosen + ' Nutrition Information')
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
