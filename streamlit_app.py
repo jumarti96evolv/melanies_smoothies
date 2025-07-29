95abc44e9ca645c1c94f87639054220a6620b4d4
jumarti96evolv <juan.martinez@evolvconsulting.com>
2025-07-29 15:50:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -39,11 +40,7 @@ if ingredients_list:
         VALUES ('""" + ingredients_string + """', '""" + name_on_order + """')
     """
 
-    #st.write(my_insert_stmt)
-    
     time_to_insert = st.button('Submit Order')
-
-    #st.write(time_to_insert)
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
