69cf21e5c29d92a368baef94179fef17de10dd43
jumarti96evolv <juan.martinez@evolvconsulting.com>
2025-07-29 16:02:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    st.write(ingredients_string)
+    # st.write(ingredients_string)
 
     my_insert_stmt = """
         INSERT INTO SMOOTHIES.PUBLIC.ORDERS (INGREDIENTS, NAME_ON_ORDER)
